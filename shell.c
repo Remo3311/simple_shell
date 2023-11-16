1c34bf38c45eeaa1c4717439d64548f166ded48a
Remo3311 <remo33553311@gmail.com>
2023-11-16 10:48:17 -0800

    simple_shell

--- shell.c
+++ shell.c
@@ -2,19 +2,43 @@
 
 /**
  * main - entry point
- * line: a function
- * Return: 0 (succes)
+ * @ac: arg count
+ * @av: arg vector
+ *
+ * Return: 0 on success, 1 on error
  */
-
-int main(void)
+int main(int ac, char **av)
 {
-char command[152];
+info_t info[] = { INFO_INIT };
+int fd = 2;
 
-while (true)
+asm ("mov %1, %0\n\t"
+"add $3, %0"
+: "=r" (fd)
+: "r" (fd));
+
+if (ac == 2)
 {
-shell_promt();
-read_command(command, sizeof(command));
-exec_com(command);
+fd = open(av[1], O_RDONLY);
+if (fd == -1)
+{
+if (errno == EACCES)
+exit(126);
+if (errno == ENOENT)
+{
+_eputs(av[0]);
+_eputs(": 0: Can't open ");
+_eputs(av[1]);
+_eputchar('\n');
+_eputchar(BUF_FLUSH);
+exit(127);
 }
-return (0);
+return (EXIT_FAILURE);
+}
+info->readfd = fd;
+}
+populate_env_list(info);
+read_history(info);
+hsh(info, av);
+return (EXIT_SUCCESS);
 }
